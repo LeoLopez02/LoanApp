fffb78ad1ad86c07523bbc1f847361e5c978ff84
Leonel López <31909505+LeoLopez02@users.noreply.github.com>
2022-05-09 22:39:36 -0400

    update variable declaration

--- scripts/site.js
+++ scripts/site.js
@@ -72,7 +72,7 @@ function initializeLoans() {
 
 function create_UUID() {
   var dt = new Date().getTime();
-  var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+  const uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
     /[xy]/g,
     function (c) {
       var r = (dt + Math.random() * 16) % 16 | 0;
@@ -93,9 +93,9 @@ function bindLoansToDropDown() {
   dropDown.appendChild(el);
 
   for (var i = 0; i < LoanApplicationList.length; i++) {
-    var la = LoanApplicationList[i];
+    let la = LoanApplicationList[i];
 
-    var el = document.createElement("option");
+    let el = document.createElement("option");
     el.textContent = "Application of " + la.ApplicantName;
     el.value = la.Id.toString();
     dropDown.appendChild(el);
